07beb7a1033acb03ff1cd9c8e76329862049a20b
loraine <mukezwa@gmail.com>
2022-08-12 14:06:26 +0200

    feat: implement searching feature

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -33,7 +34,9 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find({});
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+
+  const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     data: products,
