1134335f8b4e1719d9f8f1ae8949e7bd2d438389
loraine <mukezwa@gmail.com>
2022-08-14 16:43:53 +0200

    feat: implement get user details

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -27,9 +27,25 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     user: req.user._id,
   });
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data: order,
     message: 'Order made successfully',
+  });
+});
+
+exports.getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (!order) {
+    return next(new ErrorHandler('Order not found', 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    data: order,
   });
 });
--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { newOrder } = require('../controllers/order.controller');
+const {
+  newOrder,
+  getOrderDetails,
+} = require('../controllers/order.controller');
 
 router.post('/', newOrder);
+router.get('/:id', getOrderDetails);
 
 module.exports = router;
